031736e7d453fed4f498668cc51727700ad09951
Jackson Chen <jchen@sayhello.com>
2017-04-11 16:16:48 -0700

    enable test server using rsyslog udp format 52424
    * to test on ubuntu (rsyslogd)
    * 1.  edit /etc/rsyslog.d/50-default.conf
    *     + $ModLoad imudp
    *     + $UDPServerRun 514
    *     -*.*;auth,authpriv.none		-/var/log/syslog
    *     + *.*;auth,authpriv.none		@127.0.0.1:514;RSYSLOG_SyslogProtocol23Format
    * 2.  service rsyslog restart
    * 3.  go build client/syslog_server && sudo go run syslog_server

---
+++ client/syslog_server.go
@@ -0,0 +1,27 @@
+package main
+import "gopkg.in/mcuadros/go-syslog.v2"
+import "fmt"
+
+func main() {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+	listenErr := server.ListenUDP("0.0.0.0:514")
+	err := server.Boot()
+	if err != nil || listenErr != nil {
+		fmt.Println("Error is", err)
+		fmt.Println("Error is", listenErr)
+	}
+
+	go func(channel syslog.LogPartsChannel) {
+		for logParts := range channel {
+			fmt.Println(logParts)
+		}
+	}(channel)
+	server.Wait()
+	fmt.Printf("Server Exit")
+}
+
+
