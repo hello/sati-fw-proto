bf84843e2563d63b54c5e9d513081c242f7246bd
Jackson Chen <jchen@sayhello.com>
2017-04-19 16:43:16 -0700

    pub sub works

--- client/zmq_receiver.go
+++ client/zmq_receiver.go
@@ -1,14 +1,64 @@
 package main
+
 import (
-	zmq "github.com/pebble/zmq4"
 	"fmt"
+	"log"
 	"time"
+	"github.com/hello/sati-fw-proto/greeter"
+	zmq "github.com/pebble/zmq4"
 )
 
-func ZMQPeriodicLoop(out chan<- *greeter.HelloRequest) {
+func ExamplePub() {
+	pub, err := zmq.NewSocket(zmq.PUB)
+	if err != nil {
+		log.Fatal("ZMG!!!")
+		return
+	}
+	defer pub.Close()
+	if err := pub.Bind("ipc:///tmp/hello"); err != nil {
+		log.Fatal("Conn Error", err)
+		return
+	}
+	tick := time.Tick(time.Millisecond * 1000)
+	for {
+		select {
+		case <- tick:
+			if _,err := pub.Send(fmt.Sprint("time %s", time.Now().String()), 0); err != nil {
+				log.Fatal(err)
+				break
+			}
+		}
+	}
+
+}
+func ExampleSub(out chan<- *greeter.HelloRequest) {
+	receiver, err := zmq.NewSocket(zmq.SUB)
+	if err != nil {
+		log.Fatal("ZMG!!!")
+		return
+	}
+	defer receiver.Close()
+	if err := receiver.Connect("ipc:///tmp/hello"); err != nil {
+		log.Fatal("Conn Error", err)
+		return
+	}
+	if err := receiver.SetSubscribe("time"); err != nil {
+		log.Fatal("Sub Error", err)
+		return
+	}
+	for {
+		msg, err := receiver.Recv(0)
+		if err != nil {
+			break
+		} else {
+			fmt.Println(msg)
+		}
+	}
 
 }
 
 func main() {
-
+	c := make(chan *greeter.HelloRequest, 100)
+	go ExampleSub(c)
+	ExamplePub()
 }
